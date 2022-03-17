a176fc3e5e76005a21c511db37115c5a4d7089f6
luiizsilverio <luiiz.silverio@gmail.com>
2022-03-17 09:02:17 -0300

    upload de imagens

--- app/Controllers/Http/ExerciseController.js
+++ app/Controllers/Http/ExerciseController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Exercise = use("App/Models/Exercise")
+const Helpers = use("Helpers")
 
 class ExerciseController {
   async index({ request }) {
@@ -15,7 +16,7 @@ class ExerciseController {
     return exercise
   }
 
-  async store({ request }) {
+  async store({ request, response }) {
     // request.body retorna todo o corpo da requisição
     // request.only permite especificar os campos do corpo da requisição
     const data = request.only([
@@ -25,7 +26,24 @@ class ExerciseController {
       "waiting_time"
     ])
 
-    console.log(request.body.action_by)
+    const photo = request.file('image', {
+      types: ['image'], // só aceita imagens
+      size: '2mb',  // tamanho máximo
+    })
+
+    // verifica se já existe uma imagem com esse nome
+    if (photo) {
+      const image = await Exercise.findBy('url_image', photo.clientName)
+
+      if (image) {
+        return response.status(400).send({ error: "Imagem com nome duplicado."})
+      }
+
+      await photo.move(Helpers.publicPath('exercises'))
+      data.url_image = photo.clientName
+    }
+
+    // console.log(request.body.action_by)
     const exercise = await Exercise.create(data)
     return exercise
   }
